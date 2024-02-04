9efe8ac416ac8058d351548ab4f556fdb522e4a9
havard.borge.95@gmail.com <havard.borge.95@gmail.com>
2024-02-04 13:13:40 +0100

    52 2d transforms

--- Sjoko/src/Sjoko/Renderer/Shader.h
+++ Sjoko/src/Sjoko/Renderer/Shader.h
@@ -2,6 +2,8 @@
 
 #include <string>
 
+#include <glm/glm.hpp>
+
 namespace Sjoko {
 
   class Shader {
@@ -10,6 +12,10 @@ namespace Sjoko {
 
     virtual void Bind() const = 0;
     virtual void Unbind() const = 0;
+
+    virtual void SetFloat3(const std::string& name, const glm::vec3& value) = 0;
+    virtual void SetFloat4(const std::string& name, const glm::vec4& value) = 0;
+    virtual void SetMat4(const std::string& name, const glm::mat4& value) = 0;
 
     virtual const std::string& GetName() const = 0;
 
