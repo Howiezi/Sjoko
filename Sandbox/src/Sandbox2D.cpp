5d6e7682f4118fe23bb499f8222a65fea13918d5
havard.borge.95@gmail.com <havard.borge.95@gmail.com>
2024-02-04 14:16:11 +0100

    53 2d textures

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -14,7 +14,7 @@ Sandbox2D::Sandbox2D()
 
 void Sandbox2D::OnAttach()
 {
-  
+  m_CheckerboardTexture = Sjoko::Texture2D::Create("assets/textures/Checkerboard.png");
 }
 
 void Sandbox2D::OnDetach()
@@ -35,6 +35,7 @@ void Sandbox2D::OnUpdate(Sjoko::Timestep ts)
 
   Sjoko::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
   Sjoko::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
+  Sjoko::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture);
   Sjoko::Renderer2D::EndScene();
 
 }
