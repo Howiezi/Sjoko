6aabaa1107129aab11c16e315ed4978fe7b4eab6
havard.borge.95@gmail.com <havard.borge.95@gmail.com>
2024-02-18 15:18:14 +0100

    one more PR, feb 2020

--- Sjoko/src/Sjoko/Core/Log.cpp
+++ Sjoko/src/Sjoko/Core/Log.cpp
@@ -1,6 +1,7 @@
 #include "sjpch.h"
 
 #include "spdlog/sinks/stdout_color_sinks.h"
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace Sjoko {
 
@@ -9,12 +10,22 @@ namespace Sjoko {
 
   void Log::Init()
   {
-    spdlog::set_pattern("%^[%T] %n: %v%$");
-    s_CoreLogger = spdlog::stdout_color_mt("Sjoko");
-    s_CoreLogger->set_level(spdlog::level::trace);
+    std::vector<spdlog::sink_ptr> logSinks;
+    logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+    logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Sjoko.log", true));
 
-    s_ClientLogger = spdlog::stdout_color_mt("App");  
+    logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+    logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+    s_CoreLogger = std::make_shared<spdlog::logger>("SJOKO", begin(logSinks), end(logSinks));
+    spdlog::register_logger(s_CoreLogger);
+    s_CoreLogger->set_level(spdlog::level::trace);
+    s_CoreLogger->flush_on(spdlog::level::trace);
+
+    s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+    spdlog::register_logger(s_ClientLogger);
     s_ClientLogger->set_level(spdlog::level::trace);
+    s_ClientLogger->flush_on(spdlog::level::trace);
   }
 
 }
