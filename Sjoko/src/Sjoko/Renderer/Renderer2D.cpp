93d8c4581c1a20dc36021e2af10364a1404a75a5
havard.borge.95@gmail.com <havard.borge.95@gmail.com>
2024-02-25 20:15:57 +0100

    64 renderer stats

--- Sjoko/src/Sjoko/Renderer/Renderer2D.cpp
+++ Sjoko/src/Sjoko/Renderer/Renderer2D.cpp
@@ -21,9 +21,9 @@ namespace Sjoko {
   struct Renderer2DData
   {
     // Max per draw call
-    const uint32_t MaxQuads = 10000;
-    const uint32_t MaxVertices = MaxQuads * 4;
-    const uint32_t MaxIndices = MaxQuads * 6;
+    static const uint32_t MaxQuads = 10000;
+    static const uint32_t MaxVertices = MaxQuads * 4;
+    static const uint32_t MaxIndices = MaxQuads * 6;
     static const uint32_t MaxTextureSlots = 32;
 
     Ref<VertexArray> QuadVertexArray;
@@ -39,6 +39,8 @@ namespace Sjoko {
     uint32_t TextureSlotIndex = 1; // 0 is whitetexture
 
     glm::vec4 QuadVertexPositions[4];
+
+    Renderer2D::Statistics Stats;
   };
 
   static Renderer2DData s_Data;
@@ -142,6 +144,19 @@ namespace Sjoko {
     }
 
     RenderCommand::DrawIndexed(s_Data.QuadVertexArray, s_Data.QuadIndexCount);
+    
+    
+    s_Data.Stats.DrawCalls++;
+  }
+
+  void Renderer2D::FlushAndReset()
+  {
+    EndScene();
+
+    s_Data.QuadIndexCount = 0;
+    s_Data.QuadVertexBufferPtr = s_Data.QuadVertexBufferBase;
+
+    s_Data.TextureSlotIndex = 1;
   }
 
   void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color)
@@ -152,6 +167,11 @@ namespace Sjoko {
   void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color)
   {
     SJ_PROFILE_FUNCTION();
+
+    if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+    {
+      FlushAndReset();
+    }
 
     const float textureIndex = 0.0f; // white texture
     const float tilingFactor = 1.0f;
@@ -189,6 +209,8 @@ namespace Sjoko {
 
     s_Data.QuadIndexCount += 6;
 
+
+    s_Data.Stats.QuadCount++;
   }
 
   void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor, const glm::vec4& tintColor)
@@ -199,6 +221,11 @@ namespace Sjoko {
   void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor, const glm::vec4& tintColor)
   {
     SJ_PROFILE_FUNCTION();
+
+    if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+    {
+      FlushAndReset();
+    }
 
     float textureIndex = 0.0f;
 
@@ -250,6 +277,9 @@ namespace Sjoko {
     s_Data.QuadVertexBufferPtr++;
 
     s_Data.QuadIndexCount += 6;
+
+
+    s_Data.Stats.QuadCount++;
   }
 
   void Renderer2D::DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, const float rotation, const glm::vec4& color)
@@ -260,6 +290,11 @@ namespace Sjoko {
   void Renderer2D::DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, const float rotation, const glm::vec4& color)
   {
     SJ_PROFILE_FUNCTION();
+
+    if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+    {
+      FlushAndReset();
+    }
 
     const float textureIndex = 0.0f; // white texture
     const float tilingFactor = 1.0f;
@@ -298,6 +333,8 @@ namespace Sjoko {
 
     s_Data.QuadIndexCount += 6;
 
+
+    s_Data.Stats.QuadCount++;
   }
 
   void Renderer2D::DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, const float rotation, const Ref<Texture2D>& texture, float tilingFactor, const glm::vec4& tintColor)
@@ -308,6 +345,11 @@ namespace Sjoko {
   void Renderer2D::DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, const float rotation, const Ref<Texture2D>& texture, float tilingFactor, const glm::vec4& tintColor)
   {
     SJ_PROFILE_FUNCTION();
+
+    if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+    {
+      FlushAndReset();
+    }
 
     float textureIndex = 0.0f;
 
@@ -361,6 +403,22 @@ namespace Sjoko {
 
     s_Data.QuadIndexCount += 6;
 
+
+    s_Data.Stats.QuadCount++;
+  }
+
+  void Renderer2D::ResetStats()
+  {
+    SJ_PROFILE_FUNCTION();
+
+    memset(&s_Data.Stats, 0, sizeof(Renderer2D::Statistics));
+  }
+
+  Renderer2D::Statistics Renderer2D::GetStats()
+  {
+    SJ_PROFILE_FUNCTION();
+
+    return s_Data.Stats;
   }
 
 }
